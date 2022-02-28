54fba244fbfc790542479f6c03a95bd62f344b64
ruman <mohammedruman1996@gmail.com>
2022-02-28 14:36:06 +0530

    CHANGED.

--- day19java/src/day19java/ArrayListAssignment.java
+++ day19java/src/day19java/ArrayListAssignment.java
@@ -6,7 +6,7 @@ public class ArrayListAssignment {
 	
 }
 /*
-public class ArrayList <E> extends AbstractList<E>,
+public class ArrayList <E> extends AbstractList<E>
          implements List<E>, RandomAccess , Cloneable ,java.io.serialirable 
          {
 	//private 
--- day19java/src/day19java/LinkedListAssignmnt.java
+++ day19java/src/day19java/LinkedListAssignmnt.java
@@ -1,10 +1,19 @@
 package day19java;
 
 public class LinkedListAssignmnt {
+	
+}
 
+	/*
+	public class LinkedList<E> extends AbstractSequentialList<E>
+    implements List<E>, Dequeue<E> , Cloneable ,java.io.serialirable 
+    {
+//private 
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   
 	}
 
 }
+*/
